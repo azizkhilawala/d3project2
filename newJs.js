f93fe3278240e1283cd9da55084fa934b81caa78
Aziz Khilawala <khilawalaaziz@gmail.com>
2016-11-01 02:13:22 -0400

    Revert "Merge remote-tracking branch 'origin/master'"
    # Conflicts:
    #	newJs.js

--- newJs.js
+++ newJs.js
@@ -2080,9 +2080,6 @@ function pieChart(pienumber) {
     var svg1 = d3.select("#pie1");
     var svg2 = d3.select("#pie2");
     var svg3 = d3.select("#pie3");
-    var svg1 = d3.select("#pie1");
-    var svg2 = d3.select("#pie2");
-    var svg3 = d3.select("#pie3");
 
     if (dataselection.indexOf("B01003_0") == 0) {
         val = "B01003_001E";
@@ -2187,7 +2184,30 @@ function pieChart(pienumber) {
                     svg3.selectAll('g').remove();
                     drawPieChart("pie2", data)
                 });
+            /*queue()
+             .defer(d3.json, "
+             http://api.census.gov/data/2015/acs1?get=NAME,B01001_027E,B01001_028E,B01001_029E,B01001_030E,B01001_031E,B01001_032E,B01001_033E,B01001_034E,B01001_035E,B01001_036E,B01001_037E,B01001_038E,B01001_039E,B01001_040E,B01001_041E,B01001_042E,B01001_043E,B01001_044E,B01001_045E,B01001_046E,B01001_047E,B01001_048E,B01001_049E&for=" + scselection + ":*&key=576299d4bf73993515a4994ffe79fcee7fe72b09")
+             http://api.census.gov/data/2015/acs1?get=NAME,B01001_027E,B01001_028E,B01001_029E,B01001_030E,B01001_031E,B01001_032E,B01001_033E,B01001_034E,B01001_035E,B01001_036E,B01001_037E,B01001_038E,B01001_039E,B01001_040E,B01001_041E,B01001_042E,B01001_043E,B01001_044E,B01001_045E,B01001_046E,B01001_047E,B01001_048E,B01001_049E&for=state:*&key=              576299d4Bf73993515a4994ffe79fcee7fe72B09"
+             .await(function (error, data1) {
+             var sum = 0;
+             var data = [];
+             var labelarray = ["Under 5 Years", "5 to 9 Years", "10 to 14 Years", "15 to 17 Years", "18 and 19 Years", "20 Years", "21 Years", "22 to 24 Years", "25 to 29 Years", "30 to 34 Years", "35 to 39 Years", "40 to 44 Years", "45 to 49 Years", "50 to 54 Years", "55 to 59 Years", "60 and 61 Years", "62 to 64 Years", "65 and 66 Years", "67 to 69 Years", "70 to 74 Years", "75 to 79 Years", "80 to 84 Years", "85 Years and over"];
+             data1.splice(0, 1);
+             for(var i = 1;i<22;i++) {
+             data1.forEach(function (elem) {
+             if (elem[i] != null)
+             sum += parseInt(elem[i]);
+             });
+             data.push({
+             label:labelarray[i-1],
+             val: sum
+             });
+             sum = 0;
+             }
 
+             drawPieChart(pienumber, data)
+             });
+             */
         }
     } else
     if (dataselection.indexOf("B01002_0") == 0) {
@@ -2496,6 +2516,7 @@ function pieChart(pienumber) {
     } else
     if (dataselection.indexOf("B19013_0") == 0) {
         val = "B19013_001E";
+<<<<<<< HEAD
         svg1.selectAll('g').remove();
         svg2.selectAll('g').remove();
         svg3.selectAll('g').remove();
@@ -2787,6 +2808,8 @@ if (dataselection.indexOf("B06009_0") == 0) {
                 drawPieChart("pie2", data3)
             });
 
+=======
+>>>>>>> parent of 4e371ef... Merge remote-tracking branch 'origin/master'
     } else
     if (dataselection.indexOf("B19301_0") == 0) {
         val = "B19301_001E";
